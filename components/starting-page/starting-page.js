fcf40275272926b694a5804e877768d8250ef509
aurelc20 <acangonji20@gmail.com>
2026-05-14 18:03:19 +0200

    starter app

--- components/starting-page/starting-page.js
+++ components/starting-page/starting-page.js
@@ -1,11 +1,11 @@
-import classes from './starting-page.module.css';
+import classes from "./starting-page.module.css";
 
 function StartingPageContent() {
   // Show Link to Login page if NOT auth
 
   return (
     <section className={classes.starting}>
-      <h1>Welcome on Board!</h1>
+      <h1>Welcome on NextAuthjs (V4)!</h1>
     </section>
   );
 }
